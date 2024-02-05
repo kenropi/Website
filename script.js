3eb6a1f355f710ba991eb24daa15e8791ecb7f61
kenropi <146070365+kenropi@users.noreply.github.com>
2024-02-05 21:11:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function response(isYes) {
+    if (isYes) {
+        alert("Yay! I'm so happy you said yes hunny. Can't wait for our Valentine's date!");
+    } else {
+        alert("No worries hun, We are dating after all tehee !");
+    }
+}
